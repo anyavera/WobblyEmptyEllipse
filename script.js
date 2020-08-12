c257f84c4d77d65c4395632fde1fc8ffe561d360
anyavera <neverova.anya@gmail.com>
2020-08-12 00:41:16 +0000

    function add(num1, num2)

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function sayHi() {
+function sayHi(name) {
   var name = 'Marat'
   console.log ('Hi', name)
   console.log ('Privet')
 }
 sayHi();
 
+function add(num1, num2){
+  console.log(num1 + num2)
+
+}
+
+add(4,6)
